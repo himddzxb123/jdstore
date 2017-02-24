052bbd804def8de467bce59a11b478af3ed30b9b
chentianwei <ctw.3@163.com>
2017-02-24 22:57:35 +0800

    change product/index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
-  
+
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
+  validates :sort, presence: true
 end
